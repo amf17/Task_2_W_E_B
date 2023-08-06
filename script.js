36e733888679812506159d3bafdcc5b2bcfbce1e
Abdulrhman Alfarawati <139582388+amf17@users.noreply.github.com>
2023-08-06 07:42:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
- // get output div reference
 var output = document.getElementById("output");
- // get action element reference
  var start = document.getElementById("start");
  var stop = document.getElementById("stop");
  var language_Rec = document.getElementById("language_Rec");
-  // new speech recognition object
  var speechRecognition = speechRecognition || webkitSpeechRecognition;
  var recognizer = new speechRecognition();
-//Language
  recognizer.lang = 'ar';
 
  language_Rec.onchange = function() {
      recognizer.lang = language_Rec.value;
  }
 
-// Start&Stop recognition
  start.onclick = function() {
      recognizer.start();
  }
@@ -24,7 +19,6 @@ var output = document.getElementById("output");
  recognizer.onspeechend = function() {
      stop.click();
  }
- // This runs when the speech recognition service returns result
  recognizer.onresult = function(event) {
      var Textbox = event.results[0][0].transcript;
      output.value = Textbox;
